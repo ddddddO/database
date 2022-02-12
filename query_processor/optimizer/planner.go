2faf8522dd083db597071249d54aecae8a39e37f
ddddddO <lbfdeatq@gmail.com>
2022-02-12 18:13:41 +0900

    green test for buildPlan

--- query_processor/optimizer/planner.go
+++ query_processor/optimizer/planner.go
@@ -1,8 +1,6 @@
 package optimizer
 
 import (
-	"fmt"
-
 	"github.com/ddddddO/database/query_processor/parser"
 )
 
@@ -30,22 +28,43 @@ const (
 )
 
 func Optimize(statement *parser.Statement) (*plan, error) {
-	fmt.Println("Optimize")
-	// FIXME: 渡ってきたstatementの中のparsedにどんな種類のblockか(e.g. from句なのかwhere句なのかとか)のtypeを持たせた方がいいかも
-	plan, err := buildPlan(statement)
-	if err != nil {
-		return nil, err
-	}
-
 	// TODO: https://oss-db.jp/dojo/dojo_info_10 によると、実行計画(plan)はランダムにサンプリングしたデータを元にした推定値であるそう。
 	//       なので一回はテーブルにアクセスするということっぽい？
 	// FIXME:だとすると、ストレージエンジンの実装をしなくてはならなそう。一旦、簡易的に実行計画structに値を入れて返してその先にいったあとでここに戻ってくる
 
+	plan, err := buildPlan(statement)
+	if err != nil {
+		return nil, err
+	}
 	return plan, nil
 }
 
 func buildPlan(statement *parser.Statement) (*plan, error) {
-	p := &plan{}
+	r := getRelation(statement.Parseds)
+
+	p := &plan{
+		nodeType:     seqScan, // 仮置き
+		relationName: r,       // NOTE: 現状、statementからここしかわからない。。
+		alias:        r,       // 仮置き
+		startupCost:  0.00,    // 仮置き
+		totalCost:    439.99,  // 仮置き
+		planRows:     20,      // 仮置き
+		planWidth:    4,       // 仮置き
+	}
 
 	return p, nil
+}
+
+func getRelation(parseds []*parser.Parsed) string {
+	var fromPhraseElements []*parser.Parsed
+	for _, elm := range parseds {
+		if elm.Kind == parser.FromPhrase {
+			fromPhraseElements = append(fromPhraseElements, elm)
+		}
+	}
+
+	if len(fromPhraseElements) >= 2 {
+		return fromPhraseElements[1].Block
+	}
+	return "" // こない想定
 }
